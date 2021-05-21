a7b71ef412738e99626a7a9a4ab941dd6c8a4315
Antoine REMY <antoine.remy@epita.fr>
2021-05-21 11:36:50 +0200

    Add test to match 100% coverage
    Signed-off-by: Antoine REMY <antoine.remy@epita.fr>

--- src/main/test/java/fr/lernejo/Sample.java
+++ src/main/test/java/fr/lernejo/Sample.java
@@ -26,4 +26,21 @@ public class Sample {
             this.func = func;
         }
     }
+
+    @Test
+    void dividing_by_zero_should_produce_an_exception() {
+        int dividend = 10;
+        int divisor = 0;
+        Assertions.assertThatExceptionOfType(DivisionByZeroException.class)
+            .isThrownBy(() -> Sample.divide(dividend, divisor));
+    }
+
+    @Test
+    void dividing_10_by_2_should_produce_5() {
+        int dividend = 10; // (1)
+        int divisor = 2;
+        int quotient = Sample.divide(dividend, divisor); // (2)
+        Assertions.assertThat(quotient).as("quotient of 10 / 2")
+            .isEqualTo(5); // (3)
+    }
 }
